bb3125bcb77f8219b3815a483df0aba2c157ca3c
Hyrum <Hyrum>
2016-01-06 20:35:58 -0600

    Initial Upload

---
+++ Lecture 80 Assignment/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Lecture_80_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKey = new System.Windows.Forms.Label();
+            this.lblValue = new System.Windows.Forms.Label();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblKey
+            // 
+            this.lblKey.AutoSize = true;
+            this.lblKey.Location = new System.Drawing.Point(9, 15);
+            this.lblKey.Name = "lblKey";
+            this.lblKey.Size = new System.Drawing.Size(53, 13);
+            this.lblKey.TabIndex = 0;
+            this.lblKey.Text = "Enter Key";
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Location = new System.Drawing.Point(9, 41);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(62, 13);
+            this.lblValue.TabIndex = 0;
+            this.lblValue.Text = "Enter Value";
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(12, 154);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(57, 13);
+            this.lblPath.TabIndex = 0;
+            this.lblPath.Text = "Enter Path";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(12, 184);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(78, 13);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "Status Label";
+            // 
+            // textBox1
+            // 
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(104, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(304, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(104, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(304, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Location = new System.Drawing.Point(104, 151);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(304, 20);
+            this.textBoxFile.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 64);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(396, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add To Dictionary";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 93);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(396, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save To Disk";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 122);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(396, 23);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "Open In Notepad";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 221);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textBoxFile);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.lblValue);
+            this.Controls.Add(this.lblKey);
+            this.Name = "Form1";
+            this.Text = "Lecture 80";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKey;
+        private System.Windows.Forms.Label lblValue;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxFile;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ Lecture 80 Assignment/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Lecture_80_Assignment
+{
+    public partial class Form1 : Form
+    {
+        // flag variables to assist with selection of text in text boxes.
+        bool box1Hasfocus;
+        bool box2HasFocus;
+
+        //Dictionary for storing pairs of values as key,value.
+        Dictionary<string, string> dict = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //event to assist with selection of text in text boxes.
+            textBox1.GotFocus += textBox1_GotFocus;
+            textBox1.MouseUp += textBox1_MouseUp;
+            textBox1.Leave += textBox1_Leave;
+            textBox2.GotFocus += textBox2_GotFocus;
+            textBox2.MouseUp += textBox2_MouseUp;
+            textBox2.Leave += textBox2_Leave;
+
+            //clear the status label until something actually happens.
+            lblStatus.Text = "";
+        }
+
+
+        void textBox1_Leave(object sender, EventArgs e)
+        {
+            box1Hasfocus = false;
+            // deselect text on the way out
+            textBox1.SelectionLength = 0;
+        }
+
+        void textBox2_Leave(object sender, EventArgs e)
+        {
+            box2HasFocus = false;
+
+            // deselect text on the way out
+            textBox2.SelectionLength = 0;
+        }
+
+        void textBox1_GotFocus(object sender, EventArgs e)
+        {
+            // Select all text only if the mouse isn't down.
+            // This makes tabbing to the textbox give focus.
+            if (MouseButtons == MouseButtons.None)
+            {
+                textBox1.SelectAll();
+                box1Hasfocus = true;
+            }
+        }
+
+        void textBox2_GotFocus(object sender, EventArgs e)
+        {
+            // Select all text only if the mouse isn't down.
+            // This makes tabbing to the textbox give focus.
+            if (MouseButtons == MouseButtons.None)
+            {
+                textBox2.SelectAll();
+                box2HasFocus = true;
+            }
+        }
+
+        void textBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            // only select the text on a mouse up event if we are just
+            // entering the box and if there is not any text selected.
+            if (!box1Hasfocus && textBox1.SelectionLength == 0)
+            {
+                box1Hasfocus = true;
+                textBox1.SelectAll();
+            }
+        }
+
+        void textBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+            // only select the text on a mouse up event if we are just
+            // entering the box and if there is not any text selected.
+            if (!box2HasFocus && textBox2.SelectionLength == 0)
+            {
+                box2HasFocus = true;
+                textBox2.SelectAll();
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // check the key and value text boxes to ensure that
+            // they are not blank. 
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                // key or value field is blank
+                lblStatus.Text = "Key and Value fields cannot be blank.";
+            }
+            else
+            {
+                // both the key and the value are not blank
+                try
+                {
+                    // add values to dictionary
+                    // if a key already exists with the same name
+                    // it will through an error.
+                    dict.Add(textBox1.Text, textBox2.Text);
+
+                    // show the user that the add was successful.
+                    lblStatus.Text = "Value added, " + dict.Count + " entries total.";
+                }
+                catch (Exception ex)
+                {
+                    // it is likely that the user entered a value that is
+                    // already used.
+                    lblStatus.Text = "Error adding value.\n" + ex.Message;
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // opening a file as "AppendText" will create a file if one 
+                // does not exist.
+                using (StreamWriter writer = File.AppendText(textBoxFile.Text))
+                {
+                    // write each pair of values starting at the bottom 
+                    // of the exiting file, or top of a new file.
+                    foreach (KeyValuePair<string, string> pair in dict)
+                    {
+                        writer.WriteLine(pair.Key + ", " + pair.Value);
+                    }
+                }
+                dict.Clear();
+                lblStatus.Text = "File saved. Dictionary cleared.";
+            }
+            catch (Exception ex)
+            {
+                // likely the user did not provided a valid path.
+                lblStatus.Text = "Error:\n" + ex.Message;
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            // check to see if the user has ented valid filename before
+            // launching notepad. If not, inform the user.
+            if (File.Exists(textBoxFile.Text))
+            { Process.Start(@"notepad.exe", textBoxFile.Text); }
+            else
+            { lblStatus.Text = "File not found, check name."; }
+        }
+    }
+}
